0b51b16b9db73514e4c0b9ba53fda784f11f87f5
lperezp <luiseperezp.10@gmail.com>
2019-07-09 12:25:30 -0500

    feat(Proceso): Se terminó el proceso.

--- assets/js/main.js
+++ assets/js/main.js
@@ -9,7 +9,23 @@ $(function() {
     e.preventDefault();
     $("#wrapper").toggleClass("toggled");
   });
+
+  // TOOLTIP
+  $('[data-toggle="tooltip"]').tooltip("show");
 });
+
+function elegir(boton) {
+  boton.toggleClass("bg-primary");
+  boton.toggleClass("bg-light");
+}
+
+// EVENTO CLICK
+/* $("#formulario .btn").click(function(e) {
+  e.preventDefault();
+  console.log("ddd");
+  $("#formulario .btn").toggleClass("bg-primary");
+  $("#formulario .btn").toggleClass("bg-light");
+}); */
 
 document.documentElement.addEventListener(
   "touchmove",
